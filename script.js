aa1931207ab4c2ef2748e0016ea1537700511d6d
Craig Barkley <37020436+tektechnologies@users.noreply.github.com>
2024-10-16 02:38:24 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/every
+
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/includes
+//haystack includes needle
+
+let str = 'My instructors shred';
+
+
+//includes
+console.log('include', str.includes('uct'));
+
+//substring
+console.log('sub string', str.substring(0,15));
+
+//chatAt
+console.log('charAT', str.charAt(16));
+
+//charCodeAt
+console.log('charAt', str.charCodeAt(0));
+console.log('charAt : h', 'h'.charCodeAt(0));
+
+//from Char Code
+console.log('fromcharcode(77)', String.fromCharCode(77));
